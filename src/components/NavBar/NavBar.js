89ab3f1cd2bc31eda108d4475b68bacf6c0d48e5
magdielnavarrete <magdiel.navarrete@ugm.cl>
2021-09-27 18:20:09 -0300

    nombre

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavBar.css';
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+            <a><li>Nombre Tienda</li></a>
+            <a><li>Home</li></a>
+            <a><li>Ofertas</li></a>
+            <a> <li>tendencias</li></a>
+            <a><li>Perfil</li></a>
+            <a><li>Carrito</li></a>
+            </ul>
+
+        </nav>
+    );
+};
+
+export default NavBar;
